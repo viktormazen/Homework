ec09cb53f4baa176cf987f5c1bbd3176b6d0c3b1
Viktor Mijalchev <47793886+viktormazen@users.noreply.github.com>
2019-03-11 23:20:35 +0100

    Class04

---
+++ Class04/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Print the date that is 3 days from now
+            //Print the date that is one month from now
+            //Print the date that is one month and 3 days from now
+            //Print 1 year and 2 months ago from today
+            //Print only the current month with words
+            //Print only the current year
+
+            DateTime dateThreeDays = DateTime.Now.AddDays(3);
+            Console.WriteLine(dateThreeDays);
+            DateTime dateOneMonth = DateTime.Now.AddMonths(1);
+            Console.WriteLine(dateOneMonth);
+            DateTime dateOneMonthAndThreeDays = DateTime.Now.AddMonths(1).AddDays(3);
+            Console.WriteLine(dateOneMonthAndThreeDays);
+            DateTime dateOneYearAndTwoMonths = DateTime.Now.AddYears(1).AddMonths(2);
+            Console.WriteLine(dateOneYearAndTwoMonths);
+            string currentMonth = DateTime.Now.ToString("MMMM");
+            Console.WriteLine(currentMonth);
+            string currentYear = DateTime.Now.ToString("yyyy");
+            Console.WriteLine(currentYear);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class04/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Make a method called AgeCalculator
+            //The method will have one input parameter, your birthday date
+            //The method should return your age
+            //Show the age of a user after he inputs a date
+            //Note: take into consideration if the birthday is today, after or before today
+
+            Console.WriteLine("Enter a year of birth(YYYY):");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a month of birth(MM):");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a day of birth(DD):");
+            int day = int.Parse(Console.ReadLine());
+            var bla = new DateTime(year, month, day);
+            Console.WriteLine($"You are {AgeCalculator(bla)} years old");
+
+
+            Console.ReadLine();
+        }
+        private static int AgeCalculator(DateTime dateOfBirth)
+        {
+            int age = 0;
+            age = DateTime.Now.Year - dateOfBirth.Year;
+            if(DateTime.Now.DayOfYear < dateOfBirth.DayOfYear)
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
