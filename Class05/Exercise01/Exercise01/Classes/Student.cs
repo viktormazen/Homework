1e9287387fcac181678221a3bef8ab1f171c47cf
Viktor Mijalchev <47793886+viktormazen@users.noreply.github.com>
2019-03-15 02:13:35 +0100

    Class05

---
+++ Class05/Exercise01/Exercise01/Classes/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01.Classes
+{
+    public class Student
+    {
+            public string Name { get; set; }
+            public string Academy { get; set; }
+            public string Group { get; set; }
+
+        public Student(string name, string academy, string group)
+        {
+            Name = name;
+            Academy = academy;
+            Group = group;
+        }
+
+        
+    }
+}
